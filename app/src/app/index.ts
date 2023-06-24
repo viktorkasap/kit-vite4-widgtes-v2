a2d9fbb72dce0b5aa9ec80b7e8bbc1d1e533d819
Victor Kasap <kasap.victor@gmail.com>
2023-06-24 11:09:45 +0300

    feat: Added calculator html and styles

--- app/src/app/index.ts
+++ app/src/app/index.ts
@@ -2,109 +2,27 @@
 
 import './index.css';
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-import Swiper, { Navigation, Pagination } from 'swiper';
-
-import { apiRequest } from 'shared/api';
-
-// region Types
-interface UserProps {
-  age: number;
-  email: string;
-  image: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  birthDate: string;
-}
-
-interface DataProps {
-  users: UserProps[];
-}
-// endregion
-
-// region View
-const initSlider = () => {
-  new Swiper('.swiper', {
-    loop: true,
-    pagination: {
-      clickable: true,
-      el: '.swiper-pagination',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-    modules: [Navigation, Pagination],
+const formHandler = (form: HTMLFormElement) => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
   });
 };
 
-const slideHtml = (item: UserProps) => {
-  return `<div class="swiper-slide">
-            <div class="user">
-                <div class="user__name">Full Name: ${item.firstName} ${item.lastName}</div>
-                <div class="user__nick">Nickname: ${item.username}</div>
-                <div class="user__age">Age: ${item.age}</div>
-                <div class="user__email">Email: ${item.email}</div>
-                <div class="user__birth">Birthday: ${item.birthDate}</div>
-                <img class="user__avatar" src="${item.image}" alt="${item.firstName} ${item.lastName}">
-            </div>
-           </div>`;
-};
+const calculator = () => {
+  const calculator = document.getElementById('calculator');
+  const form = calculator?.querySelector<HTMLFormElement>('[data-calc="form"]');
+  const result = calculator?.querySelector<HTMLDivElement>('[data-calc="result"]');
 
-const renderSlides = (wrapper: HTMLDivElement, data: DataProps) => {
-  const { users } = data;
-
-  wrapper.innerHTML = Object.values(users)
-    .map((user: UserProps) => slideHtml(user))
-    .join('');
-};
-
-const renderError = (wrapper: HTMLDivElement, error: string) => {
-  wrapper.innerHTML = `<p class="error">Something went wrong. ${error}<p>`;
-};
-// endregion
-
-// region Model
-const fetchingData = async (url: string) => {
-  try {
-    const result = await apiRequest({ url, method: 'get' });
-
-    return await result;
-  } catch (err) {
-    throw Error(err as string);
-  }
-};
-// endregion
-
-// region Controller
-export const slider = async () => {
-  const url = 'https://dummyjson.com/users';
-  const wrapper = document.getElementById('slider') as HTMLDivElement;
-  const swiperWrapper = wrapper?.querySelector<HTMLDivElement>('.swiper-wrapper');
-
-  if (!wrapper || !swiperWrapper) {
-    // eslint-disable-next-line no-console
-    console.log('Slider error: Required DOM elements not found');
-
+  if (!calculator || !form || !result) {
     return false;
   }
 
-  try {
-    const data = await fetchingData(url);
-    renderSlides(swiperWrapper, data);
-    initSlider();
-  } catch (error) {
-    renderError(wrapper, error as string);
-  }
-};
-// endregion
+  // const formData = new FormData(form);
+  // form['loan-amount']
+  formHandler(form);
 
-// initial Slider
-slider();
+  return true;
+};
+
+// init
+calculator();
