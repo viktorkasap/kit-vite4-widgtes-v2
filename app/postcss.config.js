15193442739ac6a7b09042bef5484b8a94afcd5a
Victor Kasap <kasap.victor@gmail.com>
2023-06-30 11:29:48 +0300

    upd: Updated postcss.config.js

--- app/postcss.config.js
+++ app/postcss.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   plugins: [
-    require('autoprefixer')({
-      grid: 'autoplace',
+    require('postcss-import'), // https://github.com/postcss/postcss-import
+    require('postcss-mixins'), // https://github.com/postcss/postcss-mixins
+    require('postcss-nested'), // https://github.com/postcss/postcss-nested
+    require('postcss-simple-vars'), // https://github.com/postcss/postcss-simple-vars
+    require('postcss-color-function'), // https://github.com/postcss/postcss-color-function
+    require('postcss-preset-env')({
+      // https://github.com/csstools/postcss-preset-env
+      autoprefixer: { grid: 'autoplace' },
+      stage: 0, // includes all new features of CSS
     }),
-    require('postcss-nested'),
   ],
 };
